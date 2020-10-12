c41fd2d493578e8041519d955f6963fb338b910a
cassiorossi <cassio.rossi@gmail.com>
2020-10-11 22:23:40 -0300

    Use netcat/ncat as communication channel, allow multiple arbitrary scripts in a folder
    * 1. Changed to use nc to listen all UPS triggers
    2. Changed the listner so it will be able to run scripts from other locations
    3. Created a script to start, stop and rebuild the docker
    
    * Changes from Pull Request
    
    1. Removed "&" from CMD Dockerfile
    2. Changed the proccess to use only apc event as parameter
    3. Correction on docker_install.sh - move stop option position to first
    4. Changed APC_SCR_DIR default value to "/etc/apcupsd/scripts.d"
    5. Created a new env variable DATE_MASK to be used in the output messages
    6. Handled output result from apc hook script
    7. Handled apc hook script not exist warning
    8. Changed IFS value
    9. Removed other parameters value handle from acpccontrol
    10. Changed netcat loop, more secure way was implemented.
    
    * Pull Request - #3
    
    1. Changed the way the host-trigger-check.sh check hook script result
    2. Correction of the variable DATE_MASK
    3. Correction of IFS compability
    
    * Pull Resquest - New Features
    
    1. Changed the netcat solution
    	- Created a fifo in the docker
    	- Send all events to it
    	- Read it with tail to netcat server
    	- Changed the netcat server aproach to stay reading all the time (don not miss events)
    
    2. Created more log entries
    
    * Pull requests adjusts
    
    1 - Accepted the Dockerfile CMD option
    2 - Removed the "-e" and accepted the hooked script execution approach
    3 - Changed the way of stopping the proccess - however still kept the "send quit message" approach
    
    * Header typo corretion

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM alpine:latest
 
-RUN apk update && apk add --no-cache apcupsd
-
+RUN apk update && apk add --no-cache apcupsd 
 ADD apcupsd.conf /etc/apcupsd/apcupsd.conf
-ADD doshutdown /etc/apcupsd/doshutdown
+ADD apccontrol /etc/apcupsd/apccontrol
+ADD entryPoint.sh /entryPoint.sh
 
-VOLUME [ "/etc/apcupsd", "/var/log/apcupsd" ]
-
-CMD [ "/sbin/apcupsd", "-b" ]
+CMD [ "/entryPoint.sh" ]
+EXPOSE 3551/tcp
--- host-trigger-check.sh
+++ host-trigger-check.sh
@@ -1,32 +1,61 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # This script is part of apcupsd-docker by Leroy Foerster.
-# It is designed to be run by regularly (e.g. by cron). If it reads a first line '1' in the TRIGGERFILE, it will run the 'action()' function and replace the '1' with a '0'.
-# Please change the content of the TRIGGERFILE variable and the action() function according to your needs. This has to be run on the host, not within a container, to be able to shut the machine down.
 
-# Path of the file that get's created in the folder that you mapped into the apcupsd docker container
-#   Example: docker run -d -v /tmp/apcupsd-docker:/tmp/apcupsd-docker gersilex/apcupsd
-TRIGGERFILE="/tmp/apcupsd-docker/trigger"
-
-# Put everything you want to do on a shutdown condition instide this function.
+### custumize your action in a script with the same name of the action
 action(){
-  echo "Detected '1' in '$TRIGGERFILE'."
-
-  # Plan shutdown in 5 minutes, if supported by shutdown script
-  shutdown -P +5 || true
-
-  echo "Stopping all Docker containers..."
-  docker ps -q | xargs --no-run-if-empty docker stop --time 300
-
-  # Shutdown now, if we finish early with previous command
-  shutdown -P now
+  APC_EVENT=${1}
+  if [[ -f ${APC_SCR_DIR}/${APC_EVENT} ]]; then
+    SCRIPT_OUTPUT=$("${APC_SCR_DIR}/${APC_EVENT}" 2>&1)
+    SCRIPT_RETURN_CODE=$?
+    if [[ ! -z ${SCRIPT_RETURN_CODE} && ! ${SCRIPT_RETURN_CODE} -eq 0 ]]; then
+      echo "[ERROR] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - the apcd hook script for event '${APC_EVENT}' return code '$SCRIPT_RETURN_CODE'"
+    else
+      echo "[INFO] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - event apcd hook script '${APC_EVENT}' has been executed successfully"
+    fi 
+  else
+    echo "[WARN] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - can't find an hook script for apcd event '${APC_SCR_DIR}/${APC_EVENT}'"
+  fi
 }
 
+finish(){
+   echo "[INFO] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - exiting ..."
+}
+ 
+set +x
 ### Do not change below. Except you know what you are doing ###
-if [ -e "$TRIGGERFILE" ]; then
-  read first_line < $TRIGGERFILE
-  if [ "$first_line" == "1" ]; then
-    echo "0" > $TRIGGERFILE
-    action;
-  fi
+source env.list 
+if [[ "$1" == "stop" ]]; then
+  echo "[INFO] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - soft exit requested"
+  echo "quit" | nc ${DOCKER0_IP} ${DOCKER0_PORT} &
+  exit 0
 fi
+
+if [[ "$1" == "status" ]]; then
+  if ps -ef | grep ${DOCKER0_PORT} | grep "nc" | grep -v "grep" > /dev/null; then
+    echo "Service is ON"
+  else
+    echo "Service is OFF"   
+  fi
+  exit 0
+fi
+
+if [[ "$1" != "start" ]]; then
+  echo "[ERROR] valid options are: start, status and stop."
+  exit 1
+fi
+
+trap finish EXIT
+echo "[INFO] host-trigger-check.sh - "$(date +"${DATE_MASK}")" - Starting"
+coproc APC_NC { nc -l ${DOCKER0_IP} ${DOCKER0_PORT}; }
+IFS=$'\n'
+while read -r APCD_EVENT <&"${APC_NC[0]}"; do
+    case $APCD_EVENT in
+      ('quit') break ;;
+      (*) action $APCD_EVENT ;;
+    esac 
+done
+kill $APC_NC_PID
+exit 0
+
+
