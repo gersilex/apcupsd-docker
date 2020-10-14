cd8680af324d5ed012ea70fa71ed303edbace84d
Leroy Förster <gersilex@gmail.com>
2020-10-15 01:43:48 +0200

    Grammar improvement

--- host-trigger-check.sh
+++ host-trigger-check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 #
-# This script is part of apcupsd-docker by Leroy Foerster.
+# This script is part of apcupsd-docker by Leroy Förster.
 # It is designed to be run by regularly (e.g. by cron). If it reads a first line '1' in the TRIGGERFILE, it will run the 'action()' function and replace the '1' with a '0'.
 # Please change the content of the TRIGGERFILE variable and the action() function according to your needs. This has to be run on the host, not within a container, to be able to shut the machine down.
 
-# Path of the file that get's created in the folder that you mapped into the apcupsd docker container
-#   Example: docker run -d -v /tmp/apcupsd-docker:/tmp/apcupsd-docker gersilex/apcupsd
+# Path of the file that gets created in the folder that you mapped into the apcupsd docker container
+#   Example: docker run -d -v /tmp/apcupsd-docker:/tmp/apcupsd-docker gersilex/apcupsd:v1
 TRIGGERFILE="/tmp/apcupsd-docker/trigger"
 
-# Put everything you want to do on a shutdown condition instide this function.
+# Put everything you want to do on a shutdown condition inside this function.
 action(){
   echo "Detected '1' in '$TRIGGERFILE'."
 
